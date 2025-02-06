fb6f24c9e3407c15232b74b5c8681addc9d1d9a6
JinIleSon <166898497+JinIleSon@users.noreply.github.com>
2025-02-06 13:40:25 +0900

    Create ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,14 @@
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		((char *)dest)[i] = ((const char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
